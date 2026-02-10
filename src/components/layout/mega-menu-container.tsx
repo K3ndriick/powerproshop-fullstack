860100098a30b98ee96a55885f79a83656733043
Kendriick <kenjclee03@gmail.com>
2026-02-11 00:57:05 +1100

    UPDATED mega menu container
    - fixed hydrating tree error

--- src/components/layout/mega-menu-container.tsx
+++ src/components/layout/mega-menu-container.tsx
@@ -12,13 +12,7 @@ type MegaMenuContainerProps = {
 export function MegaMenuContainer({ children }: MegaMenuContainerProps) {
   return (
     <div 
-      className="absolute top-0 left-0 right-0 
-        before:content-[''] before:absolute before:h-4 before:-top-4 
-        before:left-0 before:right-0 before:bg-transparent
-        opacity-0 invisible 
-        group-hover:opacity-100 group-hover:visible 
-        transition-all duration-200 
-        pointer-events-none group-hover:pointer-events-auto"
+      className="absolute top-0 left-0 right-0 before:content-[''] before:absolute before:h-4 before:-top-4 before:left-0 before:right-0 before:bg-transparent opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 pointer-events-none group-hover:pointer-events-auto"
     >
       {/* Full-width background with border-t for subtle separation */}
       <div className="w-full bg-background shadow-lg border-t">
